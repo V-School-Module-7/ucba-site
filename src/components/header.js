be16117d10fcdbcfd5f089571fddc6eb9974f5ec
Sarah Aldous <sealdous@gmail.com>
2020-03-10 17:32:05 -0700

    mid-changes in header layout

--- src/components/header.js
+++ src/components/header.js
@@ -2,32 +2,33 @@ import React from "react"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import Logo from "../components/logo"
+import styled from 'styled-components'
+
+const HeaderContainer = styled.header`
+  background: ${props => props.bgColor};
+  margin: 0;
+`
+const LogoAndList = styled.div`
+  margin: 0;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+  font-family: Work Sans;
+  /* display: flex;
+  align-items: flex-end; */
+`
+// const StyledLink = styled(Link)`
+  
+// `
 
 const ListLink = props => (
-  <li style={{ display: `inline`,     marginRight: `1rem` }}>
+  <li style={{ display: `inline`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
   </li>
 )
 
 const Header = () => (
-  <header
-    style={{
-      background: `lavender`,
-      // marginBottom: `1.45rem`,
-      margin: `0`,
-    }}
-  >
-    <div
-      className="logoAndList"
-      style={{
-        margin: `0`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        fontFamily: `Work Sans`,
-
-      }}
-    >
-      
+  <HeaderContainer bgColor="lavender">
+    <LogoAndList>
         <div
           style={{
             maxWidth: 100, 
@@ -38,21 +39,25 @@ const Header = () => (
           style={{
             color: `white`,
             textDecoration: `none`,
+            width: `100px`,
+            height: `100px`
+            
           }}
         >
           <Logo/>
         </Link>
         </div>
         <ul style={{ listStyle: `none`, float: `right`}}>
-        <ListLink to="/">Home</ListLink>
-        <ListLink to="/report-a-swarm/">Report A Swarm</ListLink>
-        <ListLink to="/bab/">Be A Beekeeper</ListLink>
-        <ListLink to="/education/">Education</ListLink>
-        <ListLink to="/membership/">Membership</ListLink>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/report-a-swarm/">Report A Swarm</ListLink>
+          <ListLink to="/bab/">Be A Beekeeper</ListLink>
+          <ListLink to="/education/">Education</ListLink>
+          <ListLink to="/membership/">Membership</ListLink>
       </ul>
       
-    </div>
-  </header>
+      </LogoAndList>
+      
+  </HeaderContainer>
 )
 
 Header.propTypes = {
