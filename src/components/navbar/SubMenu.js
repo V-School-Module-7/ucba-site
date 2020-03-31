1e4e33ac2e51f23ec50bbc33e1cb7b95c4719b87
Sarah Aldous <sealdous@gmail.com>
2020-03-31 16:11:20 -0700

    still no transparent borders

--- src/components/navbar/SubMenu.js
+++ src/components/navbar/SubMenu.js
@@ -10,10 +10,10 @@ const SubMenuContainer = styled.ul`
     padding: 0;
     background-color: #F5D75F;
     position: absolute;
-    border-style: solid;
-    /* border-color: transparent; */
+    border: 1px solid black;
     top: 100%;
-    position: absolute; left: 0px;
+    position: absolute; 
+    left: 0px;
     &::before {
         content: "";
         position: absolute;
@@ -24,14 +24,14 @@ const SubMenuContainer = styled.ul`
         display: block;
   }
 `
-// const Container = styled.div`
-//     /* margin: 10px; */
-//     padding: 10px;
-//     border-color: transparent;
-//     background-clip: padding-box;
-    
-//     /* translate-y: -50px; */
-// `
+const Container = styled.div`
+    /* margin: 10px; */
+    /* width: 350px;
+    height: 103px; */
+    background-color: transparent;
+    /* border-color: transparent;
+    background-clip: padding-box; */
+`
 
 const SubMenuItem = styled.li`
     list-style: none;
@@ -44,21 +44,19 @@ const SubMenuItem = styled.li`
     display: flex;
     align-items: center;
     justify-content: center;
-    border: solid 1px black; 
-    /* background-clip: padding-box;
-    /* background-clip: padding;  */
-    /* background-clip: content; */ 
+    border: solid 1px black;
+    background-clip: padding; 
+    /* background-clip: content; */  
+    background-clip: content-box;
+    /* border: 10px solid rgb(100, 100, 100); 
+    border: 10px solid rgba(0, 0, 0, 0.0); */
+    /* background-clip: padding-box; */
+    /* border: 10px solid transparent; */
+    /* border: 20px solid rgba(0, 0, 0, 0.09); */
 
-    :first-child {
-        /* margin-top: 36px;     */
-    }
-    :last-child {
-        /* margin-bottom: 12px; */
-    }
     :hover {
         color: black;
         font-weight: bold;
-        /* background-color: orange; */
     }
 `
 const SubMenuLink = styled(Link)`
@@ -80,14 +78,14 @@ function SubMenu( props ) {
             name, link
         } = subMenuItem
         return (
-            // <Container>
+            <Container>
             <SubMenuItem key={i}>
                 <SubMenuLink as={Link} to={link}>
                 {name}
                    
                 </SubMenuLink>
             </SubMenuItem>
-            // </Container>
+             </Container>
         )
     })
     return <SubMenuContainer>{subMenuItems}</SubMenuContainer>
