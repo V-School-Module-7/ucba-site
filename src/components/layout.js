3a9811170396749acfcaed8cae4ceedfba3556b6
Sarah <sealdous@hotmail.com>
2020-04-16 15:17:15 -0700

    adjusting navbar file names

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Navbar from "./navbar/Desktop/Navbar.js"
+import Navbar from './navbar/Desktop/DesktopMenu.js'
 import Footer from './Footer'
 import "./layout.css"
 
@@ -45,7 +45,7 @@ const Layout = ({ children }) => {
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-        <Footer />
+      <Footer />
     </>
   )
 }
