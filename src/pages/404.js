695432c3b500aa3a0376d8b397a9230d7114aa01
Sarah <sealdous@hotmail.com>
2020-05-04 15:43:48 -0700

    adding to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,7 @@
 import React from "react"
 import styled from 'styled-components'
 import Layout from "../components/layout"
-// import bee404 from "src/images/bee404.jpg"
+import Bee404 from "src/images/bee404.jpg"
 import SEO from "../components/seo"
 
 const Page = styled.div`
@@ -38,7 +38,7 @@ margin: 10px;
 padding: 10px;
 `
 const Bee404 = styled.img`
-// https://www.instagram.com/p/BeWhES4F3g7/
+//  https://www.instagram.com/p/BeWhES4F3g7/
   width: 80%;
   grid-column: 2/3;
 `
@@ -50,13 +50,16 @@ const NotFoundPage = () => (
     <Page>
       <Container>
         <h1>PAGE NOT FOUND</h1>
-
         <ParagraphContainer>
           <p> There doesn't seem to <i>bee</i> a page here.</p>
+        </ParagraphContainer>
+        <ParagraphContainer>
           <p>What a <i>buzz</i>kill.</p>
         </ParagraphContainer>
+        <ParagraphContainer>
+          <p>To return the hive, (and avoid having us <i>drone</i> on with further bee puns) click the logo above, or <a href="/">here</a>.</p>
+        </ParagraphContainer>
       </Container>
-      
     </Page>
   </Layout >
 )
