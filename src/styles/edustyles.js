206ed7af60c56cc6bdbf5c3f9ad13bce72b024c3
Sarah <sealdous@hotmail.com>
2020-06-04 15:30:30 -0700

    completed diseases section

--- src/styles/edustyles.js
+++ src/styles/edustyles.js
@@ -100,9 +100,41 @@ margin: 2% 0 4% 0;
 
 @media (max-width: 900px) {
     width: 100%;
-    // overflow-x:hidden;
   font-size: 16px;
   margin: 2px;
   line-height: 24px;
 }
+`
+//centered, all caps, green underlined headers
+export const TextHeader = styled.h1`
+  position: relative;
+  height: 33px;
+  margin: auto;
+  display: inline-block;
+  border-bottom: 6px solid #63966a;
+  padding-bottom: 16px;
+  font-family: Work Sans;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 42px;
+  text-transform: uppercase;
+  color: #000000;
+  
+
+  @media (max-width: 900px) {
+    font-size: 31px;
+  }
+
+  @media (max-width: 550px) {
+    font-size: 20px;
+  }
+`
+//container for items you want centered
+export const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 5%;
 `
