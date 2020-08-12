a9e03ca7fdc8393b453f22ed2e4d8a8760d52309
Sarah <sealdous@hotmail.com>
2020-08-12 15:09:07 -0700

    updated Youth BAB page and ReadMe files

--- src/components/becoming-a-beekeeper/youthBAB/YouthBAB.js
+++ src/components/becoming-a-beekeeper/youthBAB/YouthBAB.js
@@ -73,12 +73,12 @@ function YouthBAB() {
           <BoxTitle>Youth Be A Beekeeper Program</BoxTitle>
         </TitleContainer>
 
-        <HeaderParagraph>To be considered for BAB, an application will be filled out and an essay must be written based on the given topic. The essay will be judged based on a rubric. The application and essay must be emailed to <Email>ut.co.beekeepers@gmail.com</Email> before 11:59 p.m. on February 25, 2020. If you are selected as a finalist, you must be in attendance at the March 12th Utah County Beekeeping Association meeting.</HeaderParagraph>
+        <HeaderParagraph>To be considered for BAB, an application will be filled out and an essay must be written based on the given topic. The essay will be judged based on a rubric. The application and essay must be emailed to <Email>ut.co.beekeepers@gmail.com</Email> before 11:59 p.m. on February 25. If you are selected as a finalist, you must be in attendance at the March Utah County Beekeeping Association meeting.</HeaderParagraph>
       </GreenLineContainer>
       <BeeHeader>Expectations</BeeHeader>
-      <ParagraphText>If selected, each participant will attend a minimum of 5 Utah County Beekeepers Association (UCBA) meetings; participate in UCBA for at least 2 years; supply 2 written reports or presentations of progress; email the BAB coordinator once a month; write and give thank you notes to BAB coordinator for sponsors no later than May 31, 2020; must participate in at least 2 speaking engagements; meet the financial obligation associated with the BAB program; and learn and have fun!</ParagraphText>
+      <ParagraphText>If selected, each participant will attend a minimum of 5 Utah County Beekeepers Association (UCBA) meetings; participate in UCBA for at least 2 years; supply 2 written reports or presentations of progress; email the BAB coordinator once a month; write and give thank you notes to BAB coordinator for sponsors no later than May 31; must participate in at least 2 speaking engagements; meet the financial obligation associated with the BAB program; and learn and have fun!</ParagraphText>
       <BeeHeader>Cost to the BAB participant </BeeHeader>
-      <ParagraphText>There will be a $100 deposit. It will be returned by January 31, 2021 to those who fulfilled the expectations (see above) of BAB. Otherwise the deposit will be forfeited. Additionally there may be costs beyond the deposit that will be required from the winner. The association will cover many costs, but may not cover all of those associated with starting a hive.</ParagraphText>
+      <ParagraphText>There will be a $100 deposit. It will be returned by January 31 of the following year to those who fulfilled the expectations (see above) of BAB. Otherwise the deposit will be forfeited. Additionally there may be costs beyond the deposit that will be required from the winner. The association will cover many costs, but may not cover all of those associated with starting a hive.</ParagraphText>
       <BeeHeader>Waiver</BeeHeader>
       <ParagraphText>Each participant's parent or guardian (because the participant will be under legal age) will sign a waiver releasing UCBA and any mentors from any liability associated with owning and operating a hive while participating in the BAB program.</ParagraphText>
 
@@ -86,7 +86,7 @@ function YouthBAB() {
       <ParagraphText>A hive with frames and bees; beekeeping mentor; a hive tool; bee brush; smoker; gloves; veil; and a state beekeeping license.</ParagraphText>
 
       <BeeHeader>Winners</BeeHeader>
-      <ParagraphText>For the 2020 year, we will have no more than 4 winners. </ParagraphText>
+      <ParagraphText>Each year, we will have no more than 4 winners. </ParagraphText>
 
       <BeeHeader>Essay for 12-14 Year-Olds</BeeHeader>
       <ParagraphText>Write a 3-5 paragraph essay. Each paragraph should include 3 but no more than 5 sentences. Write an essay including the following elements. Tell about bees. Do you have any personal experience with them? Why are you interested in beekeeping? What would you like to learn as a beekeeper during the next two years? </ParagraphText>
